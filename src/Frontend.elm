588a8958b4504a55ed500597bbb7ffc6ce6754a2
Tristan Pendergrass <tristanp@dropbox.com>
2022-01-05 14:17:36 -0800

    Initial commit

---
+++ src/Frontend.elm
@@ -0,0 +1,79 @@
+module Frontend exposing (..)
+
+import Browser exposing (UrlRequest(..))
+import Browser.Navigation as Nav
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Lamdera
+import Types exposing (..)
+import Url
+
+
+type alias Model =
+    FrontendModel
+
+
+app =
+    Lamdera.frontend
+        { init = init
+        , onUrlRequest = UrlClicked
+        , onUrlChange = UrlChanged
+        , update = update
+        , updateFromBackend = updateFromBackend
+        , subscriptions = \m -> Sub.none
+        , view = view
+        }
+
+
+init : Url.Url -> Nav.Key -> ( Model, Cmd FrontendMsg )
+init url key =
+    ( { key = key
+      , message = "Welcome to Ship Game"
+      }
+    , Cmd.none
+    )
+
+
+update : FrontendMsg -> Model -> ( Model, Cmd FrontendMsg )
+update msg model =
+    case msg of
+        UrlClicked urlRequest ->
+            case urlRequest of
+                Internal url ->
+                    ( model
+                    , Nav.pushUrl model.key (Url.toString url)
+                    )
+
+                External url ->
+                    ( model
+                    , Nav.load url
+                    )
+
+        UrlChanged url ->
+            ( model, Cmd.none )
+
+        NoOpFrontendMsg ->
+            ( model, Cmd.none )
+
+
+updateFromBackend : ToFrontend -> Model -> ( Model, Cmd FrontendMsg )
+updateFromBackend msg model =
+    case msg of
+        NoOpToFrontend ->
+            ( model, Cmd.none )
+
+
+view : Model -> Browser.Document FrontendMsg
+view model =
+    { title = ""
+    , body =
+        [ div [ style "text-align" "center", style "padding-top" "40px" ]
+            [ img [ src "https://lamdera.app/lamdera-logo-black.png", width 150 ] []
+            , div
+                [ style "font-family" "sans-serif"
+                , style "padding-top" "40px"
+                ]
+                [ text model.message ]
+            ]
+        ]
+    }
