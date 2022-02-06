598b842a52e389c9ac3d8a4394a868bbf781b2ad
tristanpendergrass <tristanpendergrass@gmail.com>
2022-02-05 22:44:23 -0800

    Preparing for v1

--- src/Evergreen/V1/Types.elm
+++ src/Evergreen/V1/Types.elm
@@ -2,17 +2,50 @@ module Evergreen.V1.Types exposing (..)
 
 import Browser
 import Browser.Navigation
+import Dict
+import Lamdera
 import Url
+
+
+type alias GameId =
+    Int
+
+
+type FrontendState
+    = BrowsingGames String
+    | ConnectingToGame GameId
+    | InGame GameId
 
 
 type alias FrontendModel =
     { key : Browser.Navigation.Key
-    , message : String
+    , state : FrontendState
+    , hasBeenGreeted : Bool
+    }
+
+
+type alias PlayerId =
+    Int
+
+
+type alias Player =
+    { id : PlayerId
+    , displayName : String
+    }
+
+
+type alias GameState =
+    { id : GameId
+    , joinCode : String
+    , players : List Player
     }
 
 
 type alias BackendModel =
-    { message : String
+    { games : Dict.Dict Int GameState
+    , players : Dict.Dict Int Player
+    , gameIdNonce : GameId
+    , playerIdNonce : PlayerId
     }
 
 
@@ -24,11 +57,16 @@ type FrontendMsg
 
 type ToBackend
     = NoOpToBackend
+    | CreateGame
+    | JoinGame String
 
 
 type BackendMsg
     = NoOpBackendMsg
+    | OnConnect Lamdera.SessionId Lamdera.ClientId
 
 
 type ToFrontend
     = NoOpToFrontend
+    | GameJoined GameState
+    | Greeting
