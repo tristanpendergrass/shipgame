980b17b52caf90f7a4f65d81f2372477897197a1
tristanpendergrass <tristanpendergrass@gmail.com>
2022-02-21 08:41:32 -0800

    Add ShipGameMsg and start the Roll update

--- src/ShipGame.elm
+++ src/ShipGame.elm
@@ -8,6 +8,7 @@ module ShipGame exposing
 import Dict exposing (Dict)
 import List.Nonempty exposing (Nonempty)
 import Player exposing (Player, PlayerId)
+import Random
 import SelectionList exposing (SelectionList)
 
 
@@ -23,7 +24,7 @@ type Ship
 type alias ShipGamePlayer =
     { id : PlayerId
     , ship : Ship
-    , score : Int
+    , pastShips : List Ship
     }
 
 
@@ -35,12 +36,96 @@ type Dice
     | RolledThrice (List ( Int, Bool )) -- <- The results of the third throw. Could have length 1 to 5
 
 
-type ShipGame
-    = ShipGame Int (SelectionList ShipGamePlayer) Dice
+type
+    ShipGame
+    -- TODO: Refactor to a record
+    = ShipGame Int (SelectionList ShipGamePlayer) Dice Random.Seed
 
 
-create : Nonempty PlayerId -> ShipGame
-create (List.Nonempty.Nonempty first rest) =
+type ShipGameMsg
+    = Roll
+    | Keep Int -- <- the index of the die to keep. Illegal indexes will be ignored
+    | Pass
+
+
+type ShipGameUpdateResult
+    = GameContinues ShipGame
+    | GameOver (List { id : PlayerId, ships : List Ship })
+
+
+rollDice : Int -> Random.Generator (List ( Int, Bool ))
+rollDice numDiceToRoll =
+    Random.list numDiceToRoll (Random.int 1 6)
+        |> Random.map (List.map (\value -> ( value, False )))
+
+
+updateDice : Dice -> ShipGame -> ShipGame
+updateDice newDice (ShipGame round players _ seed) =
+    ShipGame round players newDice seed
+
+
+updateSeed : Random.Seed -> ShipGame -> ShipGame
+updateSeed newSeed (ShipGame round players dice _) =
+    ShipGame round players dice newSeed
+
+
+shipGameUpdate : ShipGameMsg -> ShipGame -> ShipGameUpdateResult
+shipGameUpdate msg shipGame =
+    let
+        noOp =
+            GameContinues shipGame
+
+        ( dice, seed ) =
+            case shipGame of
+                ShipGame _ _ d s ->
+                    ( d, s )
+    in
+    case msg of
+        Roll ->
+            case dice of
+                RolledThrice _ ->
+                    noOp
+
+                RolledTwice diceValues ->
+                    let
+                        numDiceToRoll =
+                            List.length diceValues
+
+                        ( newDiceValues, newSeed ) =
+                            Random.step (rollDice numDiceToRoll) seed
+
+                        newDice =
+                            RolledThrice newDiceValues
+                    in
+                    GameContinues (updateDice newDice shipGame)
+
+                RolledOnce diceValues ->
+                    let
+                        numDiceToRoll =
+                            List.length diceValues
+
+                        ( newDiceValues, newSeed ) =
+                            Random.step (rollDice numDiceToRoll) seed
+
+                        newDice =
+                            RolledTwice newDiceValues
+
+                        newShipGame =
+                            shipGame
+                                |> updateDice newDice
+                                |> updateSeed newSeed
+                    in
+                    GameContinues newShipGame
+
+                _ ->
+                    Debug.todo "Implement"
+
+        _ ->
+            Debug.todo "Implement"
+
+
+create : Nonempty PlayerId -> Random.Seed -> ShipGame
+create (List.Nonempty.Nonempty first rest) initialSeed =
     let
         round =
             0
@@ -49,7 +134,7 @@ create (List.Nonempty.Nonempty first rest) =
         createPlayer playerId =
             { id = playerId
             , ship = ShipWithNothing
-            , score = 0
+            , pastShips = []
             }
 
         players =
@@ -58,13 +143,13 @@ create (List.Nonempty.Nonempty first rest) =
         dice =
             NeverRolled
     in
-    ShipGame round players dice
+    ShipGame round players dice initialSeed
 
 
 removePlayer : PlayerId -> ShipGame -> Maybe ShipGame
 removePlayer playerId shipGame =
     case shipGame of
-        ShipGame round players dice ->
+        ShipGame round players dice seed ->
             let
                 selectedPlayerWasRemoved =
                     players
@@ -79,13 +164,13 @@ removePlayer playerId shipGame =
                         dice
             in
             SelectionList.filter (.id >> (/=) playerId) players
-                |> Maybe.map (\newPlayers -> ShipGame round newPlayers newDice)
+                |> Maybe.map (\newPlayers -> ShipGame round newPlayers newDice seed)
 
 
 getPlayers : ShipGame -> Nonempty PlayerId
 getPlayers shipGame =
     case shipGame of
-        ShipGame _ playerIds _ ->
+        ShipGame _ playerIds _ _ ->
             case SelectionList.toTuple playerIds of
                 ( [], selected, last ) ->
                     List.Nonempty.Nonempty selected last
